e59ab03672954e7fe98be798e2d43ee11fb78cff
nived <nivedha.aravindmali@gmail.com>
2026-03-28 14:30:59 -0700

    Styled index.html

--- course_reg/schedule_methods.py
+++ course_reg/schedule_methods.py
@@ -1,13 +1,13 @@
 from flask import current_app
 from datetime import datetime
-
+import math
 
 
 def get_short_courses(course_codes):
     if len(course_codes) == 0:
         return []
-    
-    query = """SELECT course.course_code, course.course_name, course.department_id, course.course_number, course.type, instructor.first_name, instructor.last_name, course.is_online, course.building_code, course.room, course.cancelled, course.num_enrolled, course.capacity, course.waitlist, course.days, course.start_time, course.end_time FROM course_instructor JOIN course ON course_instructor.course_id = course.course_id JOIN instructor ON course_instructor.instructor_id = instructor.instructor_id WHERE """
+
+    query = """SELECT course.department_id, course.course_number, course.course_name, course.days, course.start_time, course.end_time, instructor.first_name, instructor.last_name, course.is_online, course.building_code, course.room FROM course_instructor JOIN course ON course_instructor.course_id = course.course_id JOIN instructor ON course_instructor.instructor_id = instructor.instructor_id WHERE """
 
     for i in range(len(course_codes)):
         if i == 0:
@@ -21,49 +21,35 @@ def get_short_courses(course_codes):
     raw_courses = cursor.fetchall()
     courses = []
 
+    # (department_id, number, name, days, start_time, end_time, teach_first, teach_last, is_online, building_code, room)
     for raw_course in raw_courses:
+        # print(raw_course)
         course = []
 
-        course.append(raw_course[0])    # course_code
-        course.append(raw_course[1])    # course_name
-
         # Abbreviation
-        cursor = current_app.db.execute("SELECT abbreviation FROM department WHERE department_id = :department_id;", {"department_id": raw_course[2]})
+        cursor = current_app.db.execute("SELECT abbreviation FROM department WHERE department_id = :department_id;", {"department_id": raw_course[0]})
         department = cursor.fetchone()[0]
-        course.append(department + " " + raw_course[3])    # department + course_number
+        course.append(department + " " + raw_course[1])    # department + course_number
 
-        course.append(raw_course[4])    # type
-        course.append(raw_course[6] + ", " + raw_course[5][0] + ".")    # last_name, first_init
+        course.append(raw_course[2])    # name/title
+        course.append(raw_course[3])    # days
 
         # Times
-        if raw_course[15] is None or raw_course[16] is None:
-            course += [None, None]
+        if raw_course[4] is None or raw_course[5] is None:
+            course.append(None)
         else:
-            start_time_raw = str(datetime.fromisoformat(raw_course[15]).hour) + ":" + str(datetime.fromisoformat(raw_course[15]).minute)
-            end_time_raw = str(datetime.fromisoformat(raw_course[16]).hour) + ":" + str(datetime.fromisoformat(raw_course[16]).minute)
-            start_time = datetime.strptime(start_time_raw, "%H:%M").strftime("%#I:%M %p")
-            end_time = datetime.strptime(end_time_raw, "%H:%M").strftime("%#I:%M %p")
-            course.append(start_time)
-            course.append(end_time)
+            start_dt = datetime.fromisoformat(raw_course[4].replace("Z", "+00:00"))
+            end_dt = datetime.fromisoformat(raw_course[5].replace("Z", "+00:00"))
+            start_time = start_dt.strftime("%#I:%M %p")  # use %#I on Windows
+            end_time = end_dt.strftime("%#I:%M %p")
+            course.append(start_time + " - " + end_time)
 
-        if (raw_course[7] == 1):
+        course.append(raw_course[7] + ", " + raw_course[6][0] + ".")    # last_name, first_init
+
+        if (raw_course[8] == 1):
             course.append("Online")
         else:
-            course.append(raw_course[8] + " " + raw_course[9])    # building_code room
-
-        # Status
-        if raw_course[10] == 1:
-            course.append("CANCELLED")
-        elif raw_course[11] < raw_course[12]:      # num_enrolled < capacity?
-            course.append("Open")
-        elif raw_course[13] >= 0:                # waitlist >= 0?
-            course.append("Waitlist")
-        elif raw_course[11] > raw_course[12]:    # num_enrolled > capacity
-            course.append("OVER")
-        else:
-            course.append("FULL")
-        
-        course.append(raw_course[14])    # days
+            course.append(raw_course[9] + " " + raw_course[10])    # building_code room
         
         courses.append(course)
     
@@ -173,76 +159,47 @@ def create_calendar(courses):
 
 
 def add_course_to_calendar(course, calendar):
-    course_len = len(course)
-    days = course[course_len - 1]
-    start_time = course[course_len - 5]
-    end_time = course[course_len - 4]
+    days = course[2]
+    time_str = course[3]
 
-    if not(days) or not(start_time) or not(end_time):
+    if not days or not time_str:
         return
 
-    
+    start_time, end_time = time_str.split(" - ")
     start_hour = datetime.strptime(start_time, "%I:%M %p").hour
     start_minute = datetime.strptime(start_time, "%I:%M %p").minute / 60
     end_hour = datetime.strptime(end_time, "%I:%M %p").hour
     end_minute = datetime.strptime(end_time, "%I:%M %p").minute / 60
     time_diff = (end_hour + end_minute) - (start_hour + start_minute)
 
-    abbreviation = course[2]
-    slot_data = (abbreviation, start_minute, round(time_diff, 2))
+    rowspan = math.ceil(round(start_minute + time_diff, 2))
+    top_pct = round((start_minute / rowspan) * 100, 2)
+    height_pct = round((time_diff / rowspan) * 100, 2)
+
+    abbreviation = course[0]
+    slot_data = (abbreviation, rowspan, top_pct, height_pct)
+    
     start_slot = -1
-
-    if 'M' in days:
-        for i in range(1, len(calendar)):
-            calendar_hour = datetime.strptime(calendar[i][0], "%I %p").hour
-
-            if start_hour == calendar_hour:
-                start_slot = i
-                calendar[start_slot][1] = slot_data
+    for i in range(1, len(calendar)):
+        if datetime.strptime(calendar[i][0], "%I %p").hour == start_hour:
+            start_slot = i
+            break
     
-    if 'Tu' in days:
-        if start_slot >= 0:
-            calendar[start_slot][2] = slot_data
-        else:
-            for i in range(1, len(calendar)):
-                calendar_hour = datetime.strptime(calendar[i][0], "%I %p").hour
-
-                if start_hour == calendar_hour:
-                    start_slot = i
-                    calendar[start_slot][2] = slot_data
+    if start_slot < 0:
+        return
     
-    if 'W' in days:
-        if start_slot >= 0:
-            calendar[start_slot][3] = slot_data
-        else:
-            for i in range(1, len(calendar)):
-                calendar_hour = datetime.strptime(calendar[i][0], "%I %p").hour
+    col_map = []
+    if 'M'  in days: col_map.append(1)
+    if 'Tu' in days: col_map.append(2)
+    if 'W'  in days: col_map.append(3)
+    if 'Th' in days: col_map.append(4)
+    if 'F'  in days: col_map.append(5)
 
-                if start_hour == calendar_hour:
-                    start_slot = i
-                    calendar[start_slot][3] = slot_data
-    
-    if 'Th' in days:
-        if start_slot >= 0:
-            calendar[start_slot][4] = slot_data
-        else:
-            for i in range(1, len(calendar)):
-                calendar_hour = datetime.strptime(calendar[i][0], "%I %p").hour
-
-                if start_hour == calendar_hour:
-                    start_slot = i
-                    calendar[start_slot][4] = slot_data
-    
-    if 'F' in days:
-        if start_slot >= 0:
-            calendar[start_slot][5] = slot_data
-        else:
-            for i in range(1, len(calendar)):
-                calendar_hour = datetime.strptime(calendar[i][0], "%I %p").hour
-
-                if start_hour == calendar_hour:
-                    start_slot = i
-                    calendar[start_slot][5] = slot_data
+    for col in col_map:
+        calendar[start_slot][col] = slot_data
+        for r in range(1, rowspan):
+            if start_slot + r < len(calendar):
+                calendar[start_slot + r][col] = "SKIP"
 
 
 def create_final_calendar(courses):
