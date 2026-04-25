7a8a21a3b586baa810cb7f679269839e6f70329f
nived <nivedha.aravindmali@gmail.com>
2026-04-24 17:55:26 -0700

    Wrote methods for calculating burnout risk

--- course_reg/logic.py
+++ course_reg/logic.py
@@ -58,3 +58,88 @@ def total_hours_per_week(courses):
         total_hours += datum[0]
     
     return total_hours
+
+
+def calculate_burnout_risk(courses, user_id):
+    burnout_score = 0
+    factors = []
+
+    query = """SELECT credits, difficulty_score FROM course WHERE """
+    for i in range(len(courses)):
+        if i == 0:
+            query += """course_code = """ + str(courses[i])
+        else:
+            query += """ OR course_code = """ + str(courses[i])
+    query += """;"""
+
+    cursor = current_app.db.execute(query)
+    course_data = cursor.fetchall()
+    cursor.close()
+
+    num_courses = 0
+    for course in course_data:
+        if course[0] > 0:
+            num_courses += 1
+
+    if num_courses >= 4:
+        burnout_score += 1
+    
+    factors.append(len(courses))
+    
+    workload = calculate_workload(courses, user_id)
+    if workload >= 36:
+        burnout_score += 3
+        factors.append("Overloaded")
+    elif workload >= 26:
+        burnout_score += 2
+        factors.append("Heavy")
+    elif workload >= 16:
+        burnout_score += 1
+        factors.append("Balanced")
+    else:
+        factors.append("Light")
+
+    num_difficult = 0
+    for course in course_data:
+        if course[1] >= 4:
+            burnout_score += 1
+            num_difficult += 1
+    
+    factors.append(num_difficult)
+    
+    if burnout_score >= 4:
+        return ("High", factors)
+    elif burnout_score >= 2:
+        return ("Medium", factors)
+    else:
+        return ("Low", factors)
+
+def generate_explanation(factors):
+    explanations = []
+
+    if factors[0] == 4:
+        explanations.append("You're taking 4 courses, which is the average amount of courses a UCI student takes, but you should still pace yourself.")
+    elif factors[0] > 4:
+        explanations.append(f"You're taking {factors[0]} courses, which is more than what the average UCI student takes. Make sure to pace yourself, and consider taking out some classes if you think it might be too much.")
+    else:
+        explanations.append(f"You're taking {factors[0]} courses, which is less than what the average UCI student takes. Consider adding some more courses")
+    
+    if factors[1] == "Overloaded":
+        explanations.append("Your schedule is overloaded! It's best to take out some of your more difficult courses.")
+    elif factors[1] == "Heavy":
+        explanations.append("Your schedule looks heavy, so consider taking out some of your more difficult courses.")
+    elif factors[1] == "Balanced":
+        explanations.append("Your schedule looks pretty balanced, but make sure to pace yourself!")
+    else:
+        explanations.append("You're schedule looks pretty light, consider adding some more difficult courses.")
+    
+    if (factors[2] / factors[0]) == 1:
+        explanations.append("You're only taking hard courses! It's best to replace a few with easier courses.")
+    elif (factors[2] / factors[0]) >= 0.75:
+        explanations.append("You're taking a lot of hard courses, consider taking out one.")
+    elif (factors[2] / factors[0]) >= 0.5:
+        explanations.append("At least half of your schedule is hard courses, make sure to pace yourself!")
+    else:
+        explanations.append("It looks like you aren't taking a lot of difficult courses, consider adding another!")
+
+    return explanations
