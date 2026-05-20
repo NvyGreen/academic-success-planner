b3612c19929d847b995e1f1097e9113d68354ef9
nived <nivedha.aravindmali@gmail.com>
2026-05-20 11:13:30 -0700

    Used negative indexing where indexing was too verbose

--- course_reg/filter_methods.py
+++ course_reg/filter_methods.py
@@ -242,8 +242,8 @@ def get_courses_adv(filters, temp_courses, reg_courses, waitlist):
 
     courses = []
     for raw_course in courses_raw:
-        added = (raw_course[len(raw_course) - 1] in temp_courses) or (raw_course[len(raw_course) - 1] in reg_courses)
-        waitlisted = raw_course[len(raw_course) - 1] in waitlist
+        added = (raw_course[-1] in temp_courses) or (raw_course[-1] in reg_courses)
+        waitlisted = raw_course[-1] in waitlist
         course = clean_course(raw_course, added, waitlisted)
         if isinstance(course, str):
             return "Error: could not fetch courses"
