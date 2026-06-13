e67ee7884f327af6cb725f510d499c132905ff9a
nived <nivedha.aravindmali@gmail.com>
2026-06-12 18:16:22 -0700

    Wrote get_metrics method

--- course_reg/analytics.py
+++ course_reg/analytics.py
@@ -4,6 +4,7 @@ from flask import current_app
 from course_reg.db import get_db
 
 
+
 def save_metrics(student_id, workload_score, burnout_score, impact_score):
     cursor = None
     try:
@@ -15,5 +16,26 @@ def save_metrics(student_id, workload_score, burnout_score, impact_score):
         raise sqlite3.Error("Error: Could not save metrics")
     finally:
         db.rollback()
+        if cursor is not None:
+            cursor.close()
+
+
+def get_metrics(student_id):
+    cursor = None
+    try:
+        db = get_db()
+        query = """SELECT COUNT(*) FROM metric WHERE student_id = :student_id;"""
+        cursor = db.execute(query, {"student_id": student_id})
+        num_schedules = cursor.fetchone()[0]
+
+        query = """SELECT workload_score, burnout_score, impact_score, timestamp FROM metric WHERE student_id = :student_id;"""
+        cursor = db.execute(query, {"student_id": student_id})
+        overtime_data = cursor.fetchall()
+
+        return (num_schedules, overtime_data)
+    except sqlite3.Error as e:
+        current_app.logger.error(f"Database error: {e}")
+        raise sqlite3.Error("Error: Could not fetch metrics")
+    finally:
         if cursor is not None:
             cursor.close()
